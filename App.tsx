636ae15dac8f817a111cce45ca91fb8d5745d53f
Khanh Le Xuan <crazycommander111@gmail.com>
2024-10-10 00:21:29 +0700

    commit 2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  launchImageLibrary,
+  type ImageLibraryOptions,
+} from "react-native-image-picker";
+import { useState, useCallback } from "react";
+import { scanFromPath } from "react-native-qr-code-image-scan";
 
 export default function App() {
+  const [qrCodes, setQrCodes] = useState<string[]>([]);
+
+  const onPress = useCallback(async () => {
+    const option: ImageLibraryOptions = {
+      mediaType: "photo",
+    };
+    const result = await launchImageLibrary(option);
+    const uri = result?.assets?.[0]?.uri;
+    if (!uri) {
+      return;
+    }
+
+    const codes = await scanFromPath(uri);
+    setQrCodes(codes);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <Text>Result: {qrCodes}</Text>
+        <Button onPress={onPress} title="Open Picker" />
+      </View>
     </View>
   );
 }
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
